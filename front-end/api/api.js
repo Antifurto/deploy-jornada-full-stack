c90abad36c56fcf45947d748fe8cfb65f837ddd9
2L-Com <llampiasi@gmail.com>
2025-02-16 18:32:18 -0300

    finalizamos o projeto

---
+++ front-end/api/api.js
@@ -0,0 +1,16 @@
+import "dotenv/config";
+// Fetch ou Axios
+import axios from "axios";
+
+ const { NODE_ENV } = process.env;
+ const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+
+//const URL = "https://deploy-jornada-full-stack.onrender.com/api";
+
+const responseArtists = await axios.get(`${URL}/artists`);
+const responseSongs = await axios.get(`${URL}/songs`);
+
+export const artistArray = responseArtists.data;
+export const songsArray = responseSongs.data;
+
+// console.log(responseArtists.data);
